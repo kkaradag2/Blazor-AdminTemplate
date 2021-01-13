3510502b883177c92a22f46cd63e5e70519f0718
Kemal Karadağ <karadagkemal76@gmail.com>
2021-01-13 10:08:41 +0300

    Add css and js files

---
+++ Blazor-AdminTemplate/Blazor-AdminTemplate/wwwroot/scripts/custom.js
@@ -0,0 +1,32 @@
+﻿
+function toggleSideBar() {
+
+    var body = document.getElementById("mainBody");
+    
+    if (body.className === "app") {
+        body.className = "app is-collapsed";
+    } else {
+        body.className = "app";
+    }
+}
+
+
+function menuItem(elementId) {
+
+    var li = document.getElementById(elementId);
+    var lu = document.getElementById(elementId + "lu");
+    if (li.className === "nav-item dropdown") {
+        li.className = "nav-item dropdown open";
+        lu.style = "display: block;"
+        var siteBarMenu = document.getElementById("sidebarMenu");
+        //pos-r ps ps--active-y
+        siteBarMenu.className = "sidebar-menu scrollable pos-r ps"
+    } else {
+        li.className = "nav-item dropdown";
+        lu.style = "display: none;"
+    }
+
+    
+
+
+}
